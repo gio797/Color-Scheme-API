e0577429e03a705d3c36d6a9b39451d891b6123a
gio797 <g.muchaidze@yahoo.com>
2023-02-02 18:35:59 +0400

    Update index.js
    color save option added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const colors = document.getElementById("colors")
 const form = document.getElementById('input-form')
 
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.hex){
+        navigator.clipboard.writeText(e.target.dataset.hex)
+        alert("Copied the color: " + e.target.dataset.hex);
+    }
+})
+
 function render(data){
     let colorsHtml = ''
         for (color of data.colors){
@@ -8,7 +15,8 @@ function render(data){
             <div class="colors-wrapper">
                 <div class="col-cont" style="background-color:${color.hex.value}">
                 </div>
-                <p>${color.hex.value}</p>
+                <p class="col-hex-val" data-hex='${color.hex.value}'>
+                ${color.hex.value}</p>
             </div>`
         }
         document.getElementById("colors").innerHTML = colorsHtml
@@ -24,6 +32,7 @@ fetch('https://www.thecolorapi.com/scheme?hex=F55A5A&mode=analogic&count=5')
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
     
+    
     const targetSelector = document.getElementById('selector').value
     const targetColor = document.getElementById('color-picker').value.slice(1)
 
@@ -34,4 +43,5 @@ form.addEventListener('submit', (e) =>{
         })        
 })
 
-   
+
+
